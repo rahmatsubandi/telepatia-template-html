15c009d6dbc7b006a09ac12ed810659ca0f6ac82
rahmatsubandi <rhmtin12@gmail.com>
2021-09-08 16:17:38 +0700

    add brands, library owl,aos and change navbar

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+AOS.init();
+
+$("#owl-carousel").owlCarousel({
+  items: 4,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  dots: false,
+  smartSpeed: 800,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+
+    600: {
+      items: 3,
+    },
+
+    1024: {
+      items: 4,
+    },
+
+    1366: {
+      items: 4,
+    },
+  },
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
